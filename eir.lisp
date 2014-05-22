5bd7d8b56d8f8159b415332b443a0655dbbafce6
Marcus Pemer <mpemer@gmail.com>
2014-05-22 23:39:06 +0200

    Added documentation strings

--- eir.lisp
+++ eir.lisp
@@ -1,7 +1,9 @@
 (in-package :eir)
 
 (defmacro seth (hash-table key value)
+"Macro that does the semi-awkward (setf (gethash ... pattern"
   `(setf (gethash ,key ,hash-table) ,value))
 
 (defmacro def (name &body body)
+  "Macro that provides a shorter version of defparameter"
   `(defparameter ,name ,@body))
